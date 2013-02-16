fe99409436b7424acabde203721d90199e080e29
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-02-15 23:29:06 -0500

    added functions for html forms

--- camacho_luis_project1/main.js
+++ camacho_luis_project1/main.js
@@ -12,39 +12,61 @@
 
 var firstname = document.getElementById("firstname");
 var lastname = document.getElementById("lastname");
-var newClient = document.getElementById("newClient");
+var clientStatus = document.forms[0].client;
 var existingClient = document.getElementById("existingClient");
 var immigrationLaw = document.getElementById("immigrationLaw");
-var businnessFormation = document.getElementById("businnessFormation");
+var businessFormation = document.getElementById("businessFormation");
 var documentDrafting = document.getElementById("documentDrafting");
+var caseSelection = document.forms[0].caseType;
 var firstConsult = document.getElementById("firstConsult");
 var payment = document.getElementById("payment");
 var clientFeedback = document.getElementById("clientFeedback");
 var rating = document.getElementById("rating");
 
-var results = function(){
+var results0 = function() {
+    for(i=0; i<caseSelection.length; i++){
+    if(caseSelection[i].checked){
+        console.log(caseSelection[i].value)
+    }
+    }
+
+};
+var results1 = function() {
+    for(i=0; i<clientStatus.length; i++){
+        if(clientStatus[i].checked){
+            console.log(clientStatus[i].value)
+        }
+    }
+
+};
+var results = function() {
+console.log();
 
 
-}
+
+
+};
+
+
 
 firstname.addEventListener("blur", results);
 lastname.addEventListener("blur", results);
-newClient.addEventListener("blur", results);
-existingClient.addEventListener("blur", results);
-immigrationLaw.addEventListener("blur", results);
-businnessFormation.addEventListener("blur", results);
-documentDrafting.addEventListener("blur", results);
+//newClient.addEventListener("blur", results);
+//existingClient.addEventListener("blur", results);
+//immigrationLaw.addEventListener("blur", results);
+//businessFormation.addEventListener("blur", results);
+//documentDrafting.addEventListener("blur", results);
 firstConsult.addEventListener("blur", results);
 payment.addEventListener("blur", results);
 clientFeedback.addEventListener("blur", results);
-rating.addEventListener("blur", results);
+rating.addEventListener("change", results);
+buttonProcess.addEventListener("click", results1);
+buttonProcess.addEventListener("click", results0);
 
 
-console.log(firstname);
 
 
-var myCheckbox = document.getElementsByTagName("li")
-console.log(myCheckbox);
+
 
 
 
