3d24a2303287f172d2a5ddb9863c30a11fcbeb43
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-02-24 20:45:13 -0500

    Changed comments

---
+++ camacho_luis_project3/js/json.js
@@ -0,0 +1,37 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Project31312
+ * Date: 2/17/13
+ * Time: 10:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+// Author: Luis Camacho Jr.
+// Created for: VFW 1302
+// Project 3
+
+var json = {
+    "contact1":{
+        "fname":["First Name:", "Luis"],
+        "lname":["Last Name:", "Camacho Jr"],
+        "ename":["email:", "LuisCamachoJr@fullsail.edu"],
+        "pnumber":["Phone Number:", "243-123-4545"],
+        "status":["Client Status:", "New Client"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-3-18"],
+        "payment":["Method of Payment:", "Travelers Check"],
+        "notes":["Client Comments:", "Great App"],
+        "app":["Rate App:", "10"]
+    },
+    "contact2":{
+        "fname":["First Name:", "John"],
+        "lname":["Last Name:", "Smith"],
+        "ename":["email:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "747-488-1212"],
+        "status":["Client Status:", "Existing Client"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-4-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Great App"],
+        "app":["Rate App:", "5"]
+    }
+};
